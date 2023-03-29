f7d7fcd2860637485ee5fac67e4ed3fa44e57ae2
Zpeidong <128704864+Zpeidong@users.noreply.github.com>
2023-03-30 01:31:42 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -585,7 +585,7 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>ChatGPT Next111</div>
           <div className={styles["sidebar-sub-title"]}>
             Build your own AI assistant.
           </div>
