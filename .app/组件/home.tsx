cadd2607f22845c4471d7e1e1ef0761a069f8c28
Zpeidong <128704864+Zpeidong@users.noreply.github.com>
2023-03-30 01:48:07 +0800

    Update and rename app/components/home.tsx to .app/组件/home.tsx

--- .app/组件/home.tsx
+++ .app/组件/home.tsx
@@ -585,7 +585,7 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>new car</div>
           <div className={styles["sidebar-sub-title"]}>
             Build your own AI assistant.
           </div>
